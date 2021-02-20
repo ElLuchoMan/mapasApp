17aa86b6e0148095722ffba7f17b68f034fc29e5
ElLuchoMan <luchoman96@gmail.com>
2021-02-20 18:29:26 -0500

    Agregado zoom

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,23 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
-  styles: [`.mapa-container{height:100%; width:100%;}.row{background-color:white; border-radius:10px; bottom:50px; left:50px; padding:10px; position:fixed; z-index:999;}`]
+  styles: [`.mapa-container{height:100%; width:100%;}.row{ background-color:white;width:400px; border-radius:10px; bottom:50px; left:50px; padding:10px; position:fixed; z-index:999;}`]
 })
-export class ZoomRangeComponent implements OnInit {
-
+export class ZoomRangeComponent implements AfterViewInit {
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
+  zoomLevel: number = 15;
   constructor() { }
-
-  ngOnInit(): void {
-
-    var map = new mapboxgl.Map({
-      container: 'mapa',
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [-74.07831206521888, 4.678609145339019],
-      zoom: 16,
+      zoom: this.zoomLevel,
     });
+    this.mapa.on('zoom', () => {
+      this.zoomLevel = this.mapa.getZoom();
+    })
+    this.mapa.on('zoomend', () => {
+      if (this.mapa.getZoom() > 19) {
+        this.mapa.zoomTo(19);
+      }
+    })
+  }
+
+  ngOnInit(): void {
+  }
+  zoomOut() {
+    this.mapa.zoomOut();
+  }
+  zoomIn() {
+    this.mapa.zoomIn();
+  }
+  zoomCambio(valor: string) {
+    this.mapa.zoomTo(Number(valor));
   }
 
 }
