f937ce27bbf4575954386f2054c2add9aa2177f0
ElLuchoMan <luchoman96@gmail.com>
2021-02-20 16:33:52 -0500

    Agregada librería mapbox

--- src/app/mapas/pages/full-sreen/full-sreen.component.ts
+++ src/app/mapas/pages/full-sreen/full-sreen.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-full-sreen',
   templateUrl: './full-sreen.component.html',
-  styles: [
-  ]
+  styles: [`#mapa{height:100%; width:100%;}`]
 })
 export class FullSreenComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    (mapboxgl as any).accessToken = environment.mapboxToken;
+    var map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11'
+    });
   }
 
 }
